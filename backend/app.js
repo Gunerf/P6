7eb44d165312889eaf577caf7de5bf083b30d9c6
Gunerf <schwerdtlenicolas@gmail.com>
2023-05-21 20:56:07 +0200

    user base de donees

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+
+//Mongoose connect 
+
+mongoose.connect('mongodb+srv://schwerdtlenicolas:3O78FfcGfgXfXtDy@cluster0.lgc4egv.mongodb.net/?retryWrites=true&w=majority',  
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Error CORS 'Cross Origin Resource Sharing'
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+//User model
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+const User = mongoose.model('User', userSchema)
+
+//Post api/auth/signup
+
+app.use(express.json())
+
+app.post('/api/auth/signup',(req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  const newUser = new User({email:email, password:password})
+  newUser.save()
+  .then(() => {res.status(201).json({message: 'Utilisateur créé !'})})
+  .catch((error => {res.status(400).json({  error })}))
+})
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,9 @@
-console.log('hello world')
-console.log('prout')
+const http = require('http');
+const app = require('./app');
+
+//Port connect
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
