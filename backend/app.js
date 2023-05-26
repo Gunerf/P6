009be21e4701091a77054083a01d59f7d2a27170
Gunerf <schwerdtlenicolas@gmail.com>
2023-05-26 03:30:26 +0200

    debut routes post et get

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const Thing = require('./models/thing')
 
 const app = express();
 const userRoutes = require('./routes/user')
@@ -21,6 +22,28 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id
+  const thing = new Thing({
+    ...req.body
+  })
+  thing.save()
+  .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+  .catch(error => res.status(400).json({  error }))
+})
+
+app.get('/api/sauces/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
 
 app.use('/api/auth', userRoutes)
 
