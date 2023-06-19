9e5815812a6d83ee94863b2f92ab6079c11c4e02
Gunerf <schwerdtlenicolas@gmail.com>
2023-06-19 02:57:30 +0200

    change model name + regex mail

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,39 +1,39 @@
-const Thing = require('../models/thing')
+const Sauce = require('../models/sauce')
 const fs = require('fs')
 
 exports.createSauce = (req, res, next) => {
     const sauceObjet = JSON.parse(req.body.sauce)
     delete sauceObjet._id
     delete sauceObjet._userId
-    const thing = new Thing({
+    const sauce = new Sauce({
       ...sauceObjet,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     })
-    thing.save()
+    sauce.save()
     .then(() => res.status(201).json({message: 'Objet enregistré !'}))
     .catch(error => res.status(400).json({  error }))
   }
 
 exports.getOneSauce = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
       .catch(error => res.status(404).json({ error }));
   }
 
 exports.getAllSauce = (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({ error }));
   }
 
   exports.modifySauce = (req, res ,next) => {
     if (req.file) {
-    Thing.findOne({ _id: req.params.id })
+    Sauce.findOne({ _id: req.params.id })
       .then((sauce) => {
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
-            Thing.updateOne({ _id: req.params.id }, { 
+            Sauce.updateOne({ _id: req.params.id }, { 
               name: req.body.sauce.name,
               manufacturer: req.body.sauce.manufacturer,
               description: req.body.sauce.description,
@@ -48,7 +48,7 @@ exports.getAllSauce = (req, res, next) => {
       })
       .catch((error) => res.status(400).json({ error }));
     } else {
-    Thing.updateOne({ _id: req.params.id }, {
+    Sauce.updateOne({ _id: req.params.id }, {
         name: req.body.name,
         manufacturer: req.body.manufacturer,
         description: req.body.description,
@@ -61,14 +61,14 @@ exports.getAllSauce = (req, res, next) => {
     };
 
 exports.deleteSauce = (req, res, next) => {
-  Thing.findOne({_id: req.params.id})
-  .then(thing => {
-    if(thing.userId != req.auth.userId){
+  Sauce.findOne({_id: req.params.id})
+  .then(sauce => {
+    if(sauce.userId != req.auth.userId){
       res.status(400).json({message: 'Non-autorisé'})
     }else{
-      const filename = thing.imageUrl.split('/images')[1]
+      const filename = sauce.imageUrl.split('/images')[1]
       fs.unlink(`images/${filename}`, () => {
-        Thing.deleteOne({_id: req.params.id})
+        Sauce.deleteOne({_id: req.params.id})
           .then(() => {res.status(200).json({message: 'Objet supprimé !'})})
           .catch(error => res.status(400).json({ error }))
       })
@@ -77,7 +77,7 @@ exports.deleteSauce = (req, res, next) => {
 }
 
 exports.likeSauce = (req, res ,next) => {
-  Thing.findOne({_id: req.params.id})
+  Sauce.findOne({_id: req.params.id})
     .then((sauce) => {
       const like = req.body.like
       const userId = req.auth.userId
@@ -95,7 +95,6 @@ exports.likeSauce = (req, res ,next) => {
       }
       sauce.likes = sauce.usersLiked.length
       sauce.dislikes = sauce.usersDisliked.length
-      console.log(sauce)
       sauce.save()
     })
   .then(() => res.status(201).json({message: 'Objet liké !'}))
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
+    email: {
+        type: String, 
+        required: true, 
+        unique: true,
+        validate:{
+            validator: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: "Veuillez saisir une adresse e-mail valide."
+        }
+    },
     password: {type: String, required: true}
 })
 
